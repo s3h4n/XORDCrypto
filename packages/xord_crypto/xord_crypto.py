615a0b7f791f079e74f672ba4b030f5f9cc9922b
s3h4n <shehanrweerasekara@gmail.com>
2022-03-02 18:01:41 +0530

    update

--- packages/xord_crypto/xord_crypto.py
+++ packages/xord_crypto/xord_crypto.py
@@ -4,20 +4,8 @@ import sys
 
 
 class XORDCrypto:
-    """
-    This is the helper class for encryption using XOR approach.
-    """
-
     @staticmethod
     def generate_key(size: int) -> bytes:
-        """
-        generate_key will generate an encryption key.
-
-        :param size: Size of the key(bytes)
-        :type size: int
-        :return: Encryption key.
-        :rtype: str
-        """
         try:
             return base64.b64encode(bytes(random.randbytes(size)))
         except Exception as e:
@@ -26,16 +14,6 @@ class XORDCrypto:
 
     @staticmethod
     def converter(key: str, string: str) -> str:
-        """
-        converter will convert the string to bytes and then XOR it with the key.
-
-        :param key: Encryption key.
-        :type key: str
-        :param string: String to be encrypted/decrypted.
-        :type string: str
-        :return: Encrypted/Decrypted string.
-        :rtype: str
-        """
         output = ""
         try:
             for i in range(len(string)):
@@ -47,16 +25,6 @@ class XORDCrypto:
             sys.exit(1)
 
     def encrypt(self, key: str, string: str) -> bytes:
-        """
-        encrypt will encrypt the string using XOR and base64.
-
-        :param key: Encryption key.
-        :type key: str
-        :param string: String to be encrypted.
-        :type string: str
-        :return: Encrypted string.
-        :rtype: bytes
-        """
         try:
             output = self.converter(key, string)
             return base64.b64encode(output.encode())
@@ -64,20 +32,10 @@ class XORDCrypto:
             print("Error in XORDCrypto.encrypt()\n>>>", e)
             sys.exit(1)
 
-    def decrypt(self, key: str, string: bytes) -> str:
-        """
-        decrypt will decrypt the string using XOR and base64.
-
-        :param key: Encryption key.
-        :type key: str
-        :param string: String to be decrypted.
-        :type string: bytes
-        :return: Decrypted string.
-        :rtype: str
-        """
+    def decrypt(self, key: str, string: bytes) -> bytes:
         try:
             output = base64.b64decode(string).decode()
-            return self.converter(key, output)
+            return self.converter(key, output).encode()
         except Exception as e:
             print("Error in XORDCrypto.encrypt()\n>>>", e)
             sys.exit(1)
--- src/handler.py
+++ src/handler.py
@@ -1,3 +1,4 @@
+import sys
 from ..packages import XORDCrypto
 from ..packages import FileHelper
 
@@ -8,11 +9,31 @@ class Handler:
         self.file = FileHelper()
 
     def handler(self) -> None:
-        key = self.xordc.generate_key(16).decode()
-        print("Key :", key)
-        file_path = input("Enter the path to the file: ")
-        original_data = self.file.read_file(file_path)
-        print("Original data:", original_data)
-        decrypted_data = self.xordc.decrypt(key, original_data)
-        print("Encrypted data:", decrypted_data)
-        self.file.write_file(file_path, decrypted_data)
+        while True:
+            print("Select an option:")
+            print("1. Encrypt")
+            print("2. Decrypt")
+            print("3. Generate key")
+            print("4. Exit")
+            option = input("Option: ")
+            if option == "1":
+                key_path = input("Key file: ")
+                file_path = input("File to encrypt: ")
+                key = self.file.read_file(key_path)
+                data = self.file.read_file(file_path)
+                encrypted_data = self.xordc.encrypt(key, data)
+                self.file.write_file(file_path, encrypted_data)
+            elif option == "2":
+                key_path = input("Key file: ")
+                file_path = input("File to decrypt: ")
+                key = self.file.read_file(key_path)
+                data = self.file.read_file(file_path)
+                decrypted_data = self.xordc.decrypt(key, data)
+                self.file.write_file(file_path, decrypted_data)
+            elif option == "3":
+                key = self.xordc.generate_key(16)
+                self.file.write_file("key.txt", key)
+            elif option == "4":
+                sys.exit()
+            else:
+                print("Invalid option")
